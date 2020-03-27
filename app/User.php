7ee648210b3fada6b69192593e36b1ef0b5f6b56
Bruce Robinson <3518111154@uth.edu.mx>
2020-03-26 18:42:09 -0600

    Se agregó el campo apellido al registro de usuarios

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Eloquent implements Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'last_name', 'email', 'password',
     ];
 
     /**
