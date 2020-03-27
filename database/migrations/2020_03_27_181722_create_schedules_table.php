4f9a131fd54e8bdbbf4801fd21163ef28344fb8b
Bruce Robinson <3518111154@uth.edu.mx>
2020-03-27 15:28:23 -0600

    Se crean migraciones para addresses y schedules y otras migraciones que los relacionan

--- database/migrations/2020_03_27_181722_create_schedules_table.php
+++ database/migrations/2020_03_27_181722_create_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('schedules', function (Blueprint $table) {
             //$table->increments('id');
             $table->string('name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->json('schedule_settings');
 
             //relaciones
             $table->string('address_id');
@@ -38,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('schedules');
     }
 }
