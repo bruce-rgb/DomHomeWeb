03bcbed4b038d217fb1a9554026968f57a941405
Bruce Robinson <3518111154@uth.edu.mx>
2020-03-27 18:54:04 -0600

    Se crean factories y seeders, se modifican factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use App\Address;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,12 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('password'),
         'remember_token' => Str::random(10),
+        'address_id' => Address::all()->random()->_id,
     ];
 });
