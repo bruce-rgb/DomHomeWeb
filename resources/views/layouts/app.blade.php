59c2539fc7a89f0de44350aaa2d55228a00263ad
Bruce Robinson <3518111154@uth.edu.mx>
2020-04-21 12:43:53 -0500

    Se agrega logotipo de aplicación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="{{ asset('img/icon.png') }}" width="25" height="25" class="d-inline-block align-top" alt="">
                     {{ config('app.name','DomHome') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
